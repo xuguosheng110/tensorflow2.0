cb9a6b7cff4360cf8da2eb5e4e19255554ceca2d
xgs11@qq.com <xgs11@qq.com>
2020-03-12 23:49:31 +0800

    'xgs001'

---
+++ 1.basic_operation/character_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/3/12 23:43
+# @Author : xuguosheng
+# @contact: xgs11@qq.com
+# @Site : 
+# @File : character_1.py
+# @Software: PyCharm
+#!/usr/bin/env python
+# encoding: utf-8
+
+import tensorflow as tf
+import cv2
+import numpy as np
+
