646891006a5feb79d6567bac6a2ec5505144557d
xuguosheng110 <xuguosheng110@users.noreply.github.com>
2020-03-15 23:25:05 +0800

    'xgs04'

---
+++ character_5.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#================================================================
+#
+# Copyright (C) 2020 * Ltd. All rights reserved.
+# Time : 2020/3/15 18:01
+# Author : Xuguosheng
+# contact: xgs11@qq.com
+# File : character_5.py
+# Software: PyCharm
+# Description :
+#
+#================================================================
